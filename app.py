3da834187fbc1a65f17306abeaf1301b7597345e
Hamza <hamza_k11@yahoo.com>
2025-08-19 01:39:40 +0500

    added responsiveness

--- app.py
+++ app.py
@@ -13,29 +13,29 @@ WEATHER_API = os.getenv("WEATHER_API")
 def home():
 
     # getting weather
-    # weather = requests.get(f'http://api.weatherapi.com/v1/current.json?q=Karachi&key={WEATHER_API}').json()
+    weather = requests.get(f'http://api.weatherapi.com/v1/current.json?q=Karachi&key={WEATHER_API}').json()
 
     # getting top stories
-    # headlines = requests.get(f'https://newsapi.org/v2/everything?q=Pakistan&searchIn=title,description&language=en&sortBy=publishedAt&pageSize=6&apiKey={API_KEY}').json()
+    headlines = requests.get(f'https://newsapi.org/v2/everything?q=Pakistan&searchIn=title,description&language=en&sortBy=publishedAt&pageSize=6&apiKey={API_KEY}').json()
     
     # getting trending section
-    # trendinghome = requests.get(f'https://newsapi.org/v2/top-headlines?category=general&pageSize=3&apiKey={API_KEY}').json()
+    trendinghome = requests.get(f'https://newsapi.org/v2/top-headlines?category=general&pageSize=3&apiKey={API_KEY}').json()
 
     # getting business section
-    # businesshome = requests.get(f'https://newsapi.org/v2/top-headlines?category=business&pageSize=3&apiKey={API_KEY}').json()
+    businesshome = requests.get(f'https://newsapi.org/v2/top-headlines?category=business&pageSize=3&apiKey={API_KEY}').json()
 
     # getting technology section
-    # technologyhome = requests.get(f'https://newsapi.org/v2/top-headlines?category=technology&pageSize=3&apiKey={API_KEY}').json()
+    technologyhome = requests.get(f'https://newsapi.org/v2/top-headlines?category=technology&pageSize=3&apiKey={API_KEY}').json()
 
     #getting sports section
-    # sportshome = requests.get(f'https://newsapi.org/v2/top-headlines?category=sports&pageSize=3&apiKey={API_KEY}').json()
+    sportshome = requests.get(f'https://newsapi.org/v2/top-headlines?category=sports&pageSize=3&apiKey={API_KEY}').json()
 
     #Entertainment
-    # entertainmenthome = requests.get(f'https://newsapi.org/v2/top-headlines?category=entertainment&pageSize=3&apiKey={API_KEY}').json()
+    entertainmenthome = requests.get(f'https://newsapi.org/v2/top-headlines?category=entertainment&pageSize=3&apiKey={API_KEY}').json()
 
-    # return render_template("home.html", topheadlines=headlines, weather=weather, trendinghome=trendinghome, businesshome=businesshome, technologyhome=technologyhome, sportshome=sportshome, entertainmenthome=entertainmenthome)
+    return render_template("home.html", topheadlines=headlines, weather=weather, trendinghome=trendinghome, businesshome=businesshome, technologyhome=technologyhome, sportshome=sportshome, entertainmenthome=entertainmenthome)
 
-    return render_template("test.html")
+    # return render_template("test.html")
 
 
 
